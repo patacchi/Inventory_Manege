de7e4c83b347b652fb634ccbd92f3aaae039824a
Patacchi <patacchi@patamon.jp>
2022-03-25 17:06:07 +0900

    [Modifie]CS_Lbl [Add]lbl sample csv [Modified] Speck sheet fontsize

---
+++ cs/CSharp_WinAPI_TextShow.bat
@@ -0,0 +1,23 @@
+﻿@echo off
+rem ソースファイルと同じディレクトリにこのバッチファイルを配置してください。
+rem バッチファイルを実行すると同じディレクトリにあるソースファイルがコンパイルされます。
+rem バージョン部分は各環境に置き換えて下さい。
+cd /d C:\Windows\Microsoft.NET\Framework\v4.0.30319
+
+rem set srcPath=%~dp0*.cs %~dp0*.resx %~dp0Properties\*.cs %~dp0Properties\*.resx
+set srcPath=%~dp0*.cs %~dp0Properties\*.cs
+set exePath=%~dp0Bin\Debug\%~n0.exe
+
+set dllPaths=system.dll,system.drawing.dll,system.windows.forms.dll
+
+csc.exe /t:winexe /optimize+ /out:%exePath% %srcPath% /r:%dllPaths%
+
+echo %ERRORLEVEL%
+
+if %ERRORLEVEL% == 0 (
+  goto SUCCESS
+)
+
+pause
+
+:SUCCESS
--- cs/Program.cs
+++ cs/Program.cs
@@ -30,7 +30,7 @@ namespace CSharp_WinAPI_TextShow
             }
             else
             {
-                string strExtention = System.IO.Path.GetExtension(args[0]);
+                string strExtention = System.IO.Path.GetExtension(args[0]).ToLower();
                 switch (strExtention)
                 {
                     case ".txt":
@@ -48,12 +48,32 @@ namespace CSharp_WinAPI_TextShow
                         System.Diagnostics.ProcessStartInfo psiExplzh = new System.Diagnostics.ProcessStartInfo(@"C:\Program Files\Explzh\EXPLZH.EXE", "\"" + args[0] + "\"");
                         System.Diagnostics.Process.Start(psiExplzh);
                         break;
+                    case ".lbl":
+                        //lblファイルだったら
+                        //本来のラベル出力プログラムに渡して、並列してDBに格納する処理を実装する
+                        //\HKEY_CLASSES_ROOT\.LBL
+                        //(既定) REZ_SZ "lbl_auto_file"
+                        //以下のみ変更
+                        //元々の設定の場所 \HKEY_CLASSES_ROOT\LBL_auto_file\shell\open\command
+                        //値："C:\Program Files\用品管理ラベル出力\label002.exe" %1
+                        //変更後:"C:\Program Files\LabelHelper\CSharp_WinAPI_TextShow.exe" "%1"
+                        //%1がクォートしてある点に注意、Program Files～はハードリンクで作成
+                        //System.Diagnostics.ProcessStartInfo psiLbl = new System.Diagnostics.ProcessStartInfo(@"C:\Program Files\用品管理ラベル出力\label002.exe", "\"" + args[0] + "\"");
+                        System.Diagnostics.ProcessStartInfo psiLbl = new System.Diagnostics.ProcessStartInfo(@"C:\Program Files\用品管理ラベル出力\label002.exe", args[0]);
+                        System.Diagnostics.Process.Start(psiLbl);
+                        //ここから独自の処理を実装していく
+                        //MessageBox.Show(System.IO.Path.GetFullPath(args[0]));
+                        break;
+                    default:
+                        //想定外のファイルだった場合は
+                        MessageBox.Show("想定外のファイルが引数に渡されました");
+                        return;
                 }
                 if (strExtention == ".txt")
                 {
                 }
             }
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
         }
     }
 }
