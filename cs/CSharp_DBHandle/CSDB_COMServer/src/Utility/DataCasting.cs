bbf9d372d63fea10e0a7b0aa6902156d4bef8a5d
Patacchi <patacchi@patamon.jp>
2023-06-06 17:01:40 +0900

    [Merge] Cleaup

--- cs/CSharp_DBHandle/CSDB_COMServer/src/Utility/DataCasting.cs
+++ cs/CSharp_DBHandle/CSDB_COMServer/src/Utility/DataCasting.cs
@@ -16,29 +16,6 @@ namespace CSDB_COMServer.Utility
             return;
         }
 
-/*         /// <summary>
-        /// List<Entirty>から Object[] に変換するメソッド
-        /// </summary>
-        /// <param name="arrayArg"></param>
-        /// <returns>Object[]</returns>
-        public object[] castArrayToObject(Array arrayArg)
-        {
-            if (arrayArg is null || arrayArg.Length == 0)
-            {
-                //長さ0の配列が来たら引数無しとして例外を投げる
-                throw new ArgumentNullException(nameof(castArrayToObject));
-            }
-            if (arrayArg.Rank != 2)
-            {
-                //2次元以外の配列が来たら今のところは未実装にし、例外を投げる
-                throw new NotImplementedException(nameof(this.castArrayToObject));
-            }
-            //引数の要素数分のObject配列を宣言(ローカル)
-            object[,] objLocal = new object[arrayArg.Length-1,1];
-            _arrObj2Dim = objLocal;
-            return new object[0];
-        }
- */
         /// <summary>
         /// List<Entityクラス>を受け取り、リフレクションを使用してカラム一覧と値一覧のリストを返す
         /// </summary>
@@ -84,7 +61,7 @@ namespace CSDB_COMServer.Utility
                         strlistColumns_.Add(pinfos[varPropCounter].Name);
                     }
                 }
-                //この時に、プロパティについている属性を調査し、除外対象の物はいここで落とす
+                //この時に、プロパティについている属性を調査し、除外対象の物はここで落とす
                 //currentObject配列の宣言 要素数はカラム名一覧のList.Count()より
                 object[] objarrCurrent = new object[strlistColumns_.Count()];
                 //取得したカラム名一覧をキーにして、valsを取得していく
@@ -96,25 +73,7 @@ namespace CSDB_COMServer.Utility
                     objarrCurrent[elmCounter] = currentType.GetProperty(strlistColumns_.ElementAt(elmCounter))?
                     .GetValue(elmClass) ?? string.Empty;
                 }
-/*                 for (var varPropCounter = 0 ;varPropCounter < pinfos.Count();varPropCounter++)
-                {
-                    if (isFirst)
-                    {
-                        //除外属性の有無の調査
-                        var attr = pinfos[varPropCounter].GetCustomAttribute<NotIncludingValueListAttribute>();
-                        if (attr is not null)
-                        {
-                            //除外属性がついていたら、colsには追加しないで次のループへ
-                            continue;
-                        }
-                        //最初のみcolsにカラム名を追加する
-                        strlistColumns_.Add(pinfos[varPropCounter].Name);
-                    }
-                    //valsに値をセットしていく、stringがNullだった場合は String.Emptyをセットする
-                    objarrCurrent[varPropCounter] =  pinfos[varPropCounter].GetValue(elmClass) ?? string.Empty;
-                }
- */                //ここで1回分のcolsがセットされているはずなので、Listに追加する
-                 objarrlistValues_.Add(objarrCurrent);
+                objarrlistValues_.Add(objarrCurrent);
                 //初回フラグを落とす
                 isFirst = false;
             }
